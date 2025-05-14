69972b0b7588fe5a7adf4bbcda60e6f1887becf4
arcobaleno <jymark.borja@gmail.com>
2025-05-14 08:22:01 +0800

    add: new docker init build

--- php/connection.php
+++ php/connection.php
@@ -1,3 +1,9 @@
 <?php
-$conn = new mysqli("localhost","root", "", "newdbcapstonelib");
+    require_once 'env.php';
+
+    $conn = new mysqli(getenv("DATABASE_HOST"), getenv("DATABASE_USER"), getenv("DATABASE_PASS"), getenv("DATABASE_NAME"));
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
