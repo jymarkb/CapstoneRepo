fa6c0c6ea68caf4dcc0635ae79a61a39ad2f2b41
arcobaleno <jymark.borja@gmail.com>
2025-05-14 09:21:29 +0800

    update: import package users

--- user/onHomeUser.php
+++ user/onHomeUser.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="..\bootstrap\dist\css\bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="..\bootstrap\dist\css\style.css">
-    <script src="..\bootstrap\dist\js\jquery-3.3.1.min.js"></script>
-    <script src="..\bootstrap\dist\js\bootstrap.min.js"></script>
+    <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="../node_modules/bootstrap3/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../node_modules/bootstrap3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
     
 
     <title>Capstone Library | Home</title>
